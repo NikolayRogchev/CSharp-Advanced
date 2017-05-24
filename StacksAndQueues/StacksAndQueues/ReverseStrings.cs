6afdee8ad240ab95655302b1559f831ea13ff36e
Nikolay <rogchev@gmail.com>
2017-05-24 10:59:16 +0300

    Stacks and Queues

---
+++ StacksAndQueues/StacksAndQueues/ReverseStrings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueues
+{
+    class ReverseStrings
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> result = new Stack<char>();
+            foreach (char symbol in input)
+            {
+                result.Push(symbol);
+            }
+
+            while (result.Count != 0)
+            {
+                Console.Write(result.Pop());
+            }
+            Console.WriteLine();
+        }
+    }
+}
