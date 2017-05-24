d0726cf4aaa0009554e40aaee25c80f6051094c9
Nikolay <rogchev@gmail.com>
2017-05-24 11:32:15 +0300

    Simple calculator

---
+++ StacksAndQueues/SimpleCalculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            // 2 + 5 + 10 - 2 - 1
+            string equasion = Console.ReadLine();
+            Stack<string> parts = new Stack<string>(equasion.Split(' ').Reverse());
+            double result = 0;
+            string operation = "+";
+
+            while (parts.Count > 0)
+            {
+                string currentItem = parts.Pop();
+                double currentDigit = 0;
+                if (double.TryParse(currentItem, out currentDigit))
+                {
+                    if (operation == "+")
+                    {
+                        result += currentDigit;
+                    }
+                    else
+                    {
+                        result -= currentDigit;
+                    }
+                }
+                else
+                {
+                    operation = currentItem;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
